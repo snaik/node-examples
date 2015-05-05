ff2610144953461f4e97db4680aa1ed27b341e9f
Sachin Naik <sachin.u.naik@gmail.com>
2015-05-04 18:29:58 -0700

    Chapter 1: Hello World

---
+++ simplehelloworld/helloworld.js
@@ -0,0 +1,16 @@
+/**
+ * Created by sachin on 5/4/15.
+ */
+
+var http = require('http');
+
+var server = http.createServer();
+
+server.on('request', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+})
+
+server.listen(3000);
+
+console.log('Server running at http://localhost:3000/');
